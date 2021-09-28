61b08bdf1e040c305e4a986c25504514e06726fa
tjwjdgks <tjwjdgks43@gmail.com>
2021-09-28 13:07:41 +0900

    study application

---
+++ src/main/java/seo/study/studyspringapplication/StudyspringapplicationApplication.java
@@ -0,0 +1,13 @@
+package seo.study.studyspringapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyspringapplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyspringapplicationApplication.class, args);
+    }
+
+}
