cb3545cea2def7f3d1fc9e2c8959ebf0ec39a8c0
tjwjdgks <tjwjdgks43@gmail.com>
2021-11-05 19:57:29 +0900

    test

--- src/main/java/seo/study/studyspringapplication/config/SecurityConfig.java
+++ src/main/java/seo/study/studyspringapplication/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().mvcMatchers("/favicon.ico");
-        web.ignoring()
-               .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+        web.ignoring().mvcMatchers("/node_modules/**");
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 }
